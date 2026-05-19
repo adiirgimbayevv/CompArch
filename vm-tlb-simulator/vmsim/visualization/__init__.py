feef3ac4422c7fdc219c3877644b5bc64c33c0a2
AlmatMussakhan <mussakhanzero@gmail.com>
2026-05-19 07:25:04 +0600

    person 6: fixed init.py

--- vm-tlb-simulator/vmsim/visualization/__init__.py
+++ vm-tlb-simulator/vmsim/visualization/__init__.py
@@ -1,4 +1,7 @@
-"""Visualization: CLI tracer, workloads, matplotlib plots."""
 from .tracer import format_step, render_trace, print_trace
+from .plots import plot_hit_rate_vs_capacity, plot_hit_rate_by_policy
+from .workload import sequential, random_uniform, locality_80_20, stride
 
-__all__ = ["format_step", "render_trace", "print_trace"]
+__all__ = ["format_step", "render_trace", "print_trace", 
+           "plot_hit_rate_vs_capacity", "plot_hit_rate_by_policy",
+           "sequential", "random_uniform", "locality_80_20", "stride"]
